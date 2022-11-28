4db3c5c9adb4de9f9d89929d2c38a30e2220628d
J2N8 <111210732+J2N8@users.noreply.github.com>
2022-11-28 17:50:55 -0500

    Added a search container and integrated the api

--- src/App.js
+++ src/App.js
@@ -9,19 +9,50 @@ const SEARCH_API =
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    fetch(FEATURED_API)
+    getMovies(FEATURED_API);
+  }, []);
+
+  const getMovies = (API) => {
+    fetch(API)
       .then((res) => res.json())
       .then((data) => {
         setMovies(data.results);
       });
-  }, []);
+  };
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+
+    if (searchTerm) {
+      getMovies(SEARCH_API + searchTerm);
+      setSearchTerm("");
+    }
+  };
+
+  const handleOnChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
   return (
-    <div className="movie-container">
-      {movies.length > 0 &&
-        movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+    <div>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className="search"
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
+      </header>
+      <div className="movie-container">
+        {movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
     </div>
   );
 }

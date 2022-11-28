c1cf01d44c5b8d75caf1b4343de2e8adee7a3a79
J2N8 <111210732+J2N8@users.noreply.github.com>
2022-11-28 16:31:20 -0500

    Using the data and setting up the layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Movie from "./components/Movie";
+import "./index.css";
 
 const FEATURED_API =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
@@ -18,7 +19,7 @@ function App() {
   }, []);
 
   return (
-    <div>
+    <div className="movie-container">
       {movies.length > 0 &&
         movies.map((movie) => <Movie key={movie.id} {...movie} />)}
     </div>
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "../index.css";
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const Movie = ({ title, poster_path, overview, vote_average }) => (
   <div className="movie">
-    
+    <div className="movie-header">
+      <img src={IMG_API + poster_path} alt={title} />
+      <div className="movie-info">
+        <h3>{title}</h3>
+        <span>{vote_average}</span>
+      </div>
+    </div>
   </div>
 );
 

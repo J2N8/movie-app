654c20a303ee24b534556841395049d777876d4b
J2N8 <111210732+J2N8@users.noreply.github.com>
2022-11-28 18:19:10 -0500

    Adding default image

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -11,14 +11,23 @@ const setVoteClass = (vote) => {
   } else {
     return "red";
   }
-}
+};
 
 const Movie = ({ title, poster_path, overview, vote_average }) => (
   <div className="movie">
-    <img src={IMG_API + poster_path} alt={title} />
+    <img
+      src={
+        poster_path
+          ? IMG_API + poster_path
+          : "https://images.unsplash.com/photo-1570385404967-fe4e1b48454b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+      }
+      alt={title}
+    />
     <div className="movie-info">
       <h3>{title}</h3>
-      <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
+      <span className={`tag ${setVoteClass(vote_average)}`}>
+        {vote_average}
+      </span>
     </div>
     <div className="movie-overview">
       <h2>Overview:</h2>

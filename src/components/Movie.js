a79d2576fbf67ee89bf87259988beb371b3707d4
J2N8 <111210732+J2N8@users.noreply.github.com>
2022-11-28 16:52:02 -0500

    Added hovered overview text for each movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,12 +4,14 @@ import "../index.css";
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const Movie = ({ title, poster_path, overview, vote_average }) => (
   <div className="movie">
-    <div className="movie-header">
-      <img src={IMG_API + poster_path} alt={title} />
-      <div className="movie-info">
-        <h3>{title}</h3>
-        <span>{vote_average}</span>
-      </div>
+    <img src={IMG_API + poster_path} alt={title} />
+    <div className="movie-info">
+      <h3>{title}</h3>
+      <span>{vote_average}</span>
+    </div>
+    <div className="movie-overview">
+      <h2>Overview:</h2>
+      <p>{overview}</p>
     </div>
   </div>
 );
